4f8f8e44417b12a7db215379399806f6f8c4122f
Eisenchamp <ttursynbekov07@gmail.com>
2024-07-26 18:10:27 +0500

    HW 3

--- homeWork.playground/Contents.swift
+++ homeWork.playground/Contents.swift
@@ -43,3 +43,67 @@ distanceTraveled = 54.3
 print(distanceTraveled)
 
 
+///HW #3
+///Problem 1
+let width = 10
+let height = 20
+
+let area = width * height
+print(area)
+
+let roomArea = area/2
+print(roomArea)
+
+let perimeter = width + width + height + height
+print(perimeter)
+
+///Problem 2
+print(12 % 5)
+
+///Problem 3
+let heartRate1 = 69
+let heartRate2 = 85
+let heartRate3 = 74
+
+let addedHR = heartRate1 + heartRate2 + heartRate3
+
+let averageHR = addedHR / 3
+print(averageHR)
+
+let heartRate1D : Double = Double(heartRate1)
+let heartRate2D : Double = Double(heartRate2)
+let heartRate3D : Double = Double(heartRate3)
+
+let addedHRD = heartRate1D + heartRate2D + heartRate3D
+
+let averageHRD = addedHRD / 3
+print(averageHRD)
+
+///Problem 4
+let steps1 : Double = 3467 //изменил название, так как до это использовал steps
+let goal : Double = 10000
+let percentOfGoal = steps1 / goal * 100
+print("\(percentOfGoal)%")
+
+///Problem 5
+var balance = 0
+print(balance)
+balance += 10000
+print(balance)
+balance += 20000
+print(balance)
+balance -= (balance/2)
+print(balance)
+balance *= 3
+print(balance)
+balance -= 3000
+print(balance)
+
+///Problem 6
+print(10 + 2 * 5)
+print((10 + 2) * 5)
+
+print(4 * 9 - 6 / 2)
+print(4 * (9 - 6) / 2)
+
+
