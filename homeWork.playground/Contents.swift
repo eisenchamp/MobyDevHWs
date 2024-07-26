50fbf07d9cdcb684f89dd71efc799c89e1b490a2
Eisenchamp <ttursynbekov07@gmail.com>
2024-07-26 17:50:21 +0500

    Hw 2

--- homeWork.playground/Contents.swift
+++ homeWork.playground/Contents.swift
@@ -1,6 +1,45 @@
+///HW #1
+
 print("Я сегодня изучил: ")
 print("Основы языка Swift")
 print("Как использовать Playgrounds")
 print("Doja Cat is the queen👸🏼")
 
-print("new")
+///HW #2
+///Problem 1
+var friends = 5940 //меняем с let на var
+print(friends)
+friends -= 100 //не будте работало, так как friends был константой
+print(friends)
+///Problem 2
+let goalSteps = 10000
+
+///Problem 3
+var schooling = 11
+print(schooling)
+schooling += 1
+print(schooling)
+print("Этот код работает потому что мы создали переменную schooling, которая может изменятся")
+
+///Problem 4
+var steps = 0
+print(steps)
+
+steps += 2000
+print(steps)
+print("Хорошая работа! Вы уже на пути к своей ежедневной цели")
+
+///Problem 5
+var name : String
+//print(name)
+print("print(name) не работает потому, что у name  нет никого значения")
+
+name = "Tim"
+print(name)
+
+///Problem 6
+var distanceTraveled : Double = 0
+distanceTraveled = 54.3
+print(distanceTraveled)
+
+
